5bb6d5474ac0815a92a3fbd59270e140a4b94c6a
tutu <tutu@tutanota.com>
2022-03-17 11:09:36 +0800

    refactor: update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Button } from "antd";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+          <Button type="primary">Primary Button</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
